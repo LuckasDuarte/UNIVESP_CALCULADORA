d34d9a6f8c7d948fc060c0f76b897e6b19050758
Lucas Duarte <luckasdu686@gmail.com>
2024-02-26 17:15:07 -0300

    INSERINDO VALIDAÇÕES

--- script.js
+++ script.js
@@ -6,7 +6,13 @@ document.getElementById("btn_calcular").addEventListener("click", function() {
     // Formula = MF = PROVA x 0,6 + AVA x 0,4
     var MF = (PROVA * 0.6) + (AVA * 0.4)
 
-    console.log(MF)
+    // verifica Aprovado ou reprovado
+    var status = (MF >= 5) ? "APROVADO" : "REPROVADO";
+
+    alert(`SUA NOTA FOI: ${MF.toFixed(1)} PORTANTO VOCÊ FOI ${status}`)
+
+    var AVA = document.getElementById("nota_AVA").value = "";
+    var PROVA = document.getElementById("nota_PROVA").value = "";
 })
 
 
